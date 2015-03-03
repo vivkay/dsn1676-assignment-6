2061169c01a9b3fba0266fd001f2261d7cea0f2b
Vivienne Kay <vivkay8@gmail.com>
2015-03-02 19:04:10 -0500

    adds bouncing animation to circle

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@ var $btnShowHide = $('.btn-show-hide');
 var $box = $('.box');
 var $btnMove = $('.btn-move');
 var $diamond = $('.diamond');
+var $btnCollapseExpand = $('.btn-collapse-expand');
+var $panel = $('.panel');
+var $btnBounce = $('.btn-bounce');
+var $circle = $('.circle');
 
 $btnShowHide.on('click', function () {
     $box.toggleClass('js-box-expand');
@@ -9,4 +13,13 @@ $btnShowHide.on('click', function () {
 
 $btnMove.on('click', function () {
     $diamond.toggleClass('slide-left');
+});
+
+$btnCollapseExpand.on('click', function () {
+    $panel.toggleClass('expand');
+});
+
+$btnBounce.on('click', function () {
+    $circle.addClass('bounce-in');
+    
 });
